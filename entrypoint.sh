414ecf3e81df26078ea995748785a3a25b13774f
Lukasz Raczylo <lukasz@raczylo.com>
2021-05-09 17:07:58 +0100

    Further modifications to the action part.

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ if [[ -z "$INPUT_CONFIG_FILE" ]]; then
   echo "Set the configuration file path."
   exit 1
 else
-  FLAGS+=" -c $INPUT_CONFIG_FILE"
+  FLAGS="${FLAGS} -c $INPUT_CONFIG_FILE"
 fi
 
 if [[ -z "$INPUT_REPOSITORY_URL" ]] && [[ -z "$INPUT_REPOSITORY_LOCAL" ]];
@@ -17,12 +17,13 @@ then
 fi
 
 if [[ ! -z "$INPUT_REPOSITORY_URL" ]]; then
-  FLAGS+=" -r $INPUT_REPOSITORY_URL"
+  FLAGS="${FLAGS} -r $INPUT_REPOSITORY_URL"
 fi
 
 if [[ ! -z "$INPUT_REPOSITORY_LOCAL" ]]; then
-  FLAGS+=" -l"
+  FLAGS="${FLAGS} -l"
 fi
 
 OUT_SEMVER_GEN=$(/go/src/app/semver-gen generate $FLAGS $*)
 echo "::set-output name=semantic_version::$(echo $OUT_SEMVER_GEN | sed -e 's|SEMVER ||g')"
+echo $OUT_SEMVER_GEN
